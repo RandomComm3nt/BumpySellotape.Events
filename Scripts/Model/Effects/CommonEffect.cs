d802877492d15be3075b3f583fe3c7ab8ac70f8a
Dan Armour <randomcomm3nt@hotmail.co.uk>
2021-08-18 16:57:21 +0100

    Added common effect

---
+++ Scripts/Model/Effects/CommonEffect.cs
@@ -0,0 +1,18 @@
+﻿using BumpySellotape.Events.Model.Effects;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Packages.Events.Model.Effects
+{
+    public class CommonEffect : IEffect
+    {
+        [SerializeField, FoldoutGroup("@Label"), OnValueChanged("EffectDefinitionChanged")] private /*CommonEffectDefinition*/ MonoBehaviour effectDefinition;
+
+        public string Label => effectDefinition ? effectDefinition.name : "[Common Effect]";
+
+        void IEffect.Process(ProcessingContext processingContext)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
