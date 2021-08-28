cced264577b4cffeab4ebe7ed622dbd592f58e16
Dan <RandomComm3nt@users.noreply.github.com>
2021-08-29 00:20:27 +0100

    Added some [HideReferenceObjectPicker] attributes

--- Scripts/Model/Effects/Text/DialogueActor.cs
+++ Scripts/Model/Effects/Text/DialogueActor.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
 
 namespace BumpySellotape.Events.Model.Effects.Text
 {
+    [HideReferenceObjectPicker]
     public class DialogueActor
     {
         [field: SerializeField] public string ActorName { get; private set; }
